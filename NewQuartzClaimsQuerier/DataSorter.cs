ee1765c270806ed852d30514853da67f8f73cb6d
chloebocker <chloebocker@gmail.com>
2018-06-05 22:53:29 -0700

    Fixing Excel sheets missing first DataTable row information

--- NewQuartzClaimsQuerier/DataSorter.cs
+++ NewQuartzClaimsQuerier/DataSorter.cs
@@ -13,7 +13,7 @@ namespace NewQuartzClaimsQuerier
         public static DataTable GetNewClaimsTable(DataTable fullTable, int daysInPastToCheckForNewClaims = 30)  //default is 30
         {
             //Create a DataColumn object for the column I want to filter with:
-            DataColumn recordedDate = fullTable.Columns["RECORDED"];
+            DataColumn stakeDate = fullTable.Columns["STAKE_DATE"];
 
             //create a new clone of the full dataset to be filled with only the relevant data:
             DataTable newClaimsTable = fullTable.Clone();
@@ -22,26 +22,26 @@ namespace NewQuartzClaimsQuerier
             foreach (DataRow row in fullTable.Rows)
             {
 
-                string nullableRecordedDate = row.Field<DateTime?>(recordedDate).ToString();
-                if (nullableRecordedDate != null)
+                string nullableStakeDate = row.Field<DateTime?>(stakeDate).ToString();
+                if (nullableStakeDate != null)
                 {
                     DateTime parsedDT;
-                    if (DateTime.TryParse(nullableRecordedDate, out parsedDT))  //this ensures that only properly formatted dates make it in to the result set
+                    if (DateTime.TryParse(nullableStakeDate, out parsedDT))  //this ensures that only properly formatted dates make it in to the result set
                     {
-                        DateTime newDT = DateTime.Parse(nullableRecordedDate);
+                        DateTime newDT = DateTime.Parse(nullableStakeDate);
                         string finalDT = newDT.ToString("yyyyMMdd");   //deal with case where it's not null but not in the right format either?
 
                         if (Convert.ToInt32(finalDT) >= Convert.ToInt32(DateTime.Now.AddDays(-daysInPastToCheckForNewClaims).ToString("yyyyMMdd")))
                         {
                             //include row in the filtered datatable
                             newClaimsTable.ImportRow(row);
-                            Console.WriteLine("Recent claim was recorded on: " + finalDT);
+                            Console.WriteLine("Recent claim was staked on: " + finalDT);
 
                         }
                     }
                 }
             }
-            Console.WriteLine("New claims table number of columns is: " + newClaimsTable.Columns.Count.ToString());
+            Console.WriteLine("\nNumber of new claims staked in last " + Convert.ToString(daysInPastToCheckForNewClaims) + " days is: " + newClaimsTable.Rows.Count);
             Console.ReadLine();
             return newClaimsTable;
         }
@@ -76,7 +76,7 @@ namespace NewQuartzClaimsQuerier
                     }
                 }
             }
-            Console.WriteLine("Recently expired claims table number of columns is: " + expiredClaimsTable.Columns.Count.ToString());
+            Console.WriteLine("\nNumber of claims that expired in last " + Convert.ToString(daysInPast) + " days is: " + Convert.ToString(expiredClaimsTable.Rows.Count));
             Console.ReadLine();
             return expiredClaimsTable;      
         }
@@ -109,11 +109,10 @@ namespace NewQuartzClaimsQuerier
                     }
                 }
             }
-            Console.WriteLine("Upcoming expiring claims table number of columns is: " + upcomingExpiringClaimsTable.Columns.Count.ToString());
+            Console.WriteLine("\nNumber of claims that will expire in next " + Convert.ToString(daysInFutureToCheckForExpiry) + " days is: " + Convert.ToString(upcomingExpiringClaimsTable.Rows.Count.ToString()));
             Console.ReadLine();
             return upcomingExpiringClaimsTable;
         }
-
     }
 }
 
--- NewQuartzClaimsQuerier/ExcelWriter.cs
+++ NewQuartzClaimsQuerier/ExcelWriter.cs
@@ -17,29 +17,21 @@ namespace NewQuartzClaimsQuerier
         {
             XLWorkbook wb = new XLWorkbook();
             var ws = wb.Worksheets.Add(tableType);
+            //var test = dt.Rows[0][0];
 
             //set the column headers:
             for (int j = 0; j < dt.Columns.Count; j++)
             {
                 string s = dt.Columns[j].ColumnName.ToString();
                 ws.Cell(1, j + 1).SetValue<string>(s);
-            }              
-
-            Console.WriteLine("num of columns = " + dt.Columns.Count.ToString() + ", num of rows = " + dt.Rows.Count.ToString());
-            Console.ReadLine();     
+            }                
 
             //insert all elements for rows after the column headers:
-            for (int i = 1; i < dt.Rows.Count; i++)
+            for (int i = 0; i < dt.Rows.Count; i++)
             {
                 for (int j = 0; j < dt.Columns.Count; j++)
                 {
-                    //Console.WriteLine("num of final cols is: " + dt.Columns.Count.ToString() + ", num of rows is: " + dt.Rows.Count.ToString());
-                    //Console.ReadLine();
-
-                    var element = dt.Rows[i][dt.Columns[j]].ToString();  //378, 582
-                    //Console.WriteLine("row " + i +": " + dt.Rows[i].ToString() + ", column " + j + ": " +  dt.Columns[j].ToString());
-                    //Console.ReadLine();
-
+                    var element = dt.Rows[i][dt.Columns[j]].ToString(); 
                     ws.Cell(i + 2, j + 1).SetValue<string>(element);      
                 }  
             }
