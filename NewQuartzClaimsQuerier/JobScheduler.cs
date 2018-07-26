49085b9f757dbd0c0a802ea8ffc66cfa04b5b187
chloebocker <chloebocker@gmail.com>
2018-07-26 00:55:48 -0700

    Working on scheduler

--- NewQuartzClaimsQuerier/JobScheduler.cs
+++ NewQuartzClaimsQuerier/JobScheduler.cs
@@ -17,10 +17,16 @@ namespace NewQuartzClaimsQuerier
 
             //get a scheduler:
             IScheduler scheduler = schedulerFactory.GetScheduler().Result; //added the result
-            scheduler.Start();
+            //scheduler.Start();
+
+            //? TRYING OUT:
+            JobKey jobKey1 = new JobKey("job1", "group1");
+            //IJobDetail job1 = JobBuilder.newJob(job1.class).WithIdentity(jobKey1).Build();
 
             //define the job and tie it to Job class
-            IJobDetail job = JobBuilder.Create<Job>().WithIdentity("myJob", "group1").Build(); //the WithIdentity is optional
+            //IJobDetail job = JobBuilder.Create<Job>().WithIdentity("myJob", "group1").Build(); //the WithIdentity is optional
+            IJobDetail job = JobBuilder.Create<Job>().WithIdentity(jobKey1).Build(); //the WithIdentity is optional
+
 
             //trigger the job to run now, and then every 40 seconds
             ITrigger trigger = TriggerBuilder.Create()
@@ -28,7 +34,7 @@ namespace NewQuartzClaimsQuerier
                 //my custom cron expression:  (cron is best suited, but other options to cron expression are: WithCalendarIntervalSchedule, WithDailyTimeIntervalSchedule, WithSimpleSchedule
                 //.WithCronSchedule("0 0 8 ? * 2 *")  //0 seconds, 0 minutes, 8 a.m., no specific day of month, all months, Monday, all years
                  //cron expression JUST FOR TESTING:
-                .WithCronSchedule("0 00 00 ? * 5")  //0 seconds, 0 minutes, midnight, no specific day of month, all months, Thurs
+                .WithCronSchedule("0 55 00 ? * 5")  //0 seconds, 0 minutes, midnight, no specific day of month, all months, Thurs
                 .StartAt(DateTime.UtcNow)
                 .WithPriority(1)
                 .Build();
@@ -42,12 +48,15 @@ namespace NewQuartzClaimsQuerier
             //  .RepeatForever())
             //.Build();
 
+            scheduler.Start();
+
             //tell quartz to schedule the job using my trigger
             scheduler.ScheduleJob(job, trigger);
+            //ADDED: DONT KNOW IF THIS SHOULD BE ABOVE OR BELOW:
+            //scheduler.Start();
 
             Console.WriteLine("test");
             Console.ReadLine();
-
 
         }
     }
